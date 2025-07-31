c5fed388cd2e1b21542bcca2b1d96397750d9aac
hichamsergi <hichamsergi@gmail.com>
2025-07-31 15:01:43 +0200

    remote%_calculator

--- modulo-1/horario_oficina/calculator.py
+++ modulo-1/horario_oficina/calculator.py
@@ -1,8 +1,38 @@
 # Imports:
+import pandas as pd
+import os
+import glob
+from tabula import read_pdf
 
 #ETL:
-## Extract
-## Transform
-## Load-Log
+
+## Extract:
+def extract_pdfdata():
+    '''
+    Genera un DF con datos de los PDF
+    '''
+    BASEDIR = os.path.dirname(__file__)
+
+    DATADIR = os.path.join(BASEDIR, "data")
+
+    df_alldata = pd.DataFrame()
+
+    for pdffile_data in glob.glob(os.path.join(DATADIR, "*.pdf")):
+
+        
+        df_pdf = read_pdf(pdffile_data, pages='all', stream=True)
+      
+        df_alldata = pd.concat(df_alldata, df_pdf, ignore_index=True)
+
+    return df_alldata
+
+## Transform:
+
+
+## Load-Log:
+
 
 # Calculate:
+## Ruta a los datos:
+
+print(extract_pdfdata())
